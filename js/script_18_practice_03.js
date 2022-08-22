a312df0f50415a2025bdf100a18d453428af8025
tweeqUS <kirillwalker@gmail.com>
2022-08-22 19:08:10 +0400

    practice 18(function)

---
+++ js/script_18_practice_03.js
@@ -0,0 +1,104 @@
+"use strict";
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+
+
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+    }
+}
+start();
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false,
+};
+
+
+
+function rememberMyFilm() {
+    let i = 0;
+    while (i < 2) {
+   let questionOne = prompt("Один из последних просмотренных фильмов?") ;
+   let questionTwo = prompt("На сколько оцените его?");
+   i++;
+    
+   if(questionOne != '' && questionTwo != '' && questionOne != null && questionTwo != null && questionOne.length < 50) {
+        personalMovieDB.movies[questionOne] = questionTwo; 
+   } else {
+        i--;
+   }
+}
+}
+rememberMyFilm();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count >= 1 && personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+}
+detectPersonalLevel();
+
+function writeYourGenres() {
+    let i = 0;
+    while (i < 3) {
+        let answers = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+        i++;
+        personalMovieDB.genres.push(answers);
+    }
+}
+writeYourGenres();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    } else {
+        console.log("база закрыта");
+    }
+}
+showMyDB();
+
+
+//Решение училки
+// function showMyDB(hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+// showMyDB(personalMovieDB.privat);
+
+
+
